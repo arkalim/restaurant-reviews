3f104111c4193d7d882d398a24432fcb9649244e
Abdur Rahman Kalim <arkhappiness@gmail.com>
2021-06-14 12:47:27 +0530

    started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/RestaurantsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RestaurantsList() {
+    return <div>Restaurants List</div>;
+}
+
+export default RestaurantsList;
